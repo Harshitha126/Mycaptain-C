4e0731385b1ed90baa3165df41302cbaf5a366ec
Harshitha126 <65272687+Harshitha126@users.noreply.github.com>
2020-06-09 17:39:13 +0530

    Add files via upload
    finding_size_of_datatypes
    product_of_two_numbers

---
+++ finding_size_of_datatypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    cout<<"Size of char:"<<sizeof(c)<<"\tbyte";
+    cout<<"\nSize of int:"<<sizeof(a)<<"\tbytes";
+    cout<<"\nSize of float:"<<sizeof(b)<<"\tbytes";
+    cout<<"\nSize of double:"<<sizeof(d)<<"\tbytes";
+}
